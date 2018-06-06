f6e970c077a13bb9e497407bcc1aac5f4f99c71a
Muhammad Daniel <mhmddaniel8@gmail.com>
2018-06-06 14:04:55 +0700

    updating image fetching function
    updating image fetching function

--- application/models/M_padmin.php
+++ application/models/M_padmin.php
@@ -400,6 +400,7 @@ class M_padmin extends CI_Model{
 		$this->db->select('*');
 		$this->db->from('file');
 		$this->db->where('proyek_id',$kode);
+		$this->db->order_by('proyek_id', 'DESC');
 		$hsl=$this->db->get();
 		return $hsl;
 	}
