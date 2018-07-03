83513403ca3da393c0648d3d896189f0f9546e1d
PK-PC <mhmddaniel8@gmail.com>
2018-07-03 13:06:35 +0700

    Lampiran

--- application/views/padmin/proyek/uplampiran.php
+++ application/views/padmin/proyek/uplampiran.php
@@ -8,9 +8,9 @@ body {
 
 .file-upload {
 	background-color: #ffffff;
-	width: 600px;
 	margin: 0 auto;
 	padding: 20px;
+	height: 500px;
 }
 
 .file-upload-btn {
@@ -61,6 +61,7 @@ body {
 	border: 4px dashed #1FB264;
 	position: relative;
 	margin-bottom: 50px;
+	height:400px;
 }
 
 .image-dropping,
@@ -76,13 +77,14 @@ body {
 
 .drag-text {
 	text-align: center;
+	bottom: 0;
 }
 
 .drag-text h3 {
-	font-weight: 100;
 	text-transform: uppercase;
 	color: #15824B;
-	padding: 60px 0;
+	padding-top: 300px;
+	font-size:20px;
 }
 
 .file-upload-image {
@@ -118,6 +120,106 @@ body {
 	border: 0;
 	transition: all .2s ease;
 }
+
+.padbtn{
+	margin-top: -130px;
+}
+.boxhead{
+	background: rgba(112,235,131,1);
+	background: -moz-linear-gradient(left, rgba(112,235,131,1) 0%, rgba(39,179,230,1) 100%);
+	background: -webkit-gradient(left top, right top, color-stop(0%, rgba(112,235,131,1)), color-stop(100%, rgba(39,179,230,1)));
+	background: -webkit-linear-gradient(left, rgba(112,235,131,1) 0%, rgba(39,179,230,1) 100%);
+	background: -o-linear-gradient(left, rgba(112,235,131,1) 0%, rgba(39,179,230,1) 100%);
+	background: -ms-linear-gradient(left, rgba(112,235,131,1) 0%, rgba(39,179,230,1) 100%);
+	background: linear-gradient(to right, rgba(112,235,131,1) 0%, rgba(39,179,230,1) 100%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#70eb83', endColorstr='#27b3e6', GradientType=1 );
+	height:80px;
+	padding-top:50px;
+}
+.boxhead2{
+	background: rgba(141,112,235,1);
+	background: -moz-linear-gradient(left, rgba(141,112,235,1) 0%, rgba(39,131,230,1) 100%);
+	background: -webkit-gradient(left top, right top, color-stop(0%, rgba(141,112,235,1)), color-stop(100%, rgba(39,131,230,1)));
+	background: -webkit-linear-gradient(left, rgba(141,112,235,1) 0%, rgba(39,131,230,1) 100%);
+	background: -o-linear-gradient(left, rgba(141,112,235,1) 0%, rgba(39,131,230,1) 100%);
+	background: -ms-linear-gradient(left, rgba(141,112,235,1) 0%, rgba(39,131,230,1) 100%);
+	background: linear-gradient(to right, rgba(141,112,235,1) 0%, rgba(39,131,230,1) 100%);
+	filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#8d70eb', endColorstr='#2783e6', GradientType=1 );
+	height:80px;
+	padding-top:50px;
+}
+.boxttle{
+	margin-top: -20px;
+	text-align: center;
+	color:white;
+}
+
+
+
+.file-upload-contentss {
+	display: none;
+	text-align: center;
+}
+
+.file-upload-inputss {
+	position: absolute;
+	margin: 0;
+	padding: 0;
+	width: 100%;
+	height: 100%;
+	outline: none;
+	opacity: 0;
+	cursor: pointer;
+}
+
+.image-upload-wrapss {
+	margin-top: 10px;
+	border: 4px dashed #1FB264;
+	position: relative;
+	margin-bottom: 50px;
+	height:400px;
+}
+
+.image-droppingss,
+.image-upload-wrapss:hover {
+	background-color: #1FB264;
+	border: 4px dashed #ffffff;
+}
+
+.file-upload-imagess {
+	max-height: 200px;
+	max-width: 200px;
+	margin: auto;
+	padding: 20px;
+}
+
+.remove-imagess {
+	width: 200px;
+	margin: 0;
+	color: #fff;
+	background: #cd4535;
+	border: none;
+	padding: 10px;
+	border-radius: 4px;
+	border-bottom: 4px solid #b02818;
+	transition: all .2s ease;
+	outline: none;
+	text-transform: uppercase;
+	font-weight: 700;
+}
+
+.remove-imagess:hover {
+	background: #c13b2a;
+	color: #ffffff;
+	transition: all .2s ease;
+	cursor: pointer;
+}
+
+.remove-imagess:active {
+	border: 0;
+	transition: all .2s ease;
+}
+
 </style>
 <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php echo base_url() ?>assets/bower_components/font-awesome/css/font-awesome.min.css">
@@ -132,80 +234,91 @@ body {
 
 <div class="content-wrapper">
 	<section class="content-header">
-		<h1>
-			Data Bagian
+		<h1 style="margin-left:50px">
+			<i class="fa fa-chevron-left"></i> Tambah Berkas
 			<small></small>
 		</h1>
 		<ol class="breadcrumb">
 			<li><a href="<?php echo base_url()?>padmin"><i class="fa fa-dashboard"></i> Home</a></li>
 			<li><a href="<?php echo base_url()?>padmin/proyek">Proyek</a></li>
-			<li class="active">Data Bagian</li>
+			<li class="active">Tambah Berkas</li>
 		</ol>
 	</section>
+
 	<section class="content">
-
 		<div class="row">
-
-			<div class="col-md-12">
-
-				<div class="box box-warning">
-					<div class="box-header with-border">
-						<h3 class="box-title">Foto Proyek</h3>
-					</div>
-					<script class="jsbin" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-					<div class="file-upload">
-						<form action="<?php echo base_url()?>padmin/save_lampiran_foto" method="POST" enctype="multipart/form-data" >
-
-							<div class="image-upload-wrap">
-								<input type="hidden" name="proyek_id" value="<?php echo $b['proyek_id']; ?>">
-								<input type="hidden"  name="jenis"  value="foto" class="form-control">	
-								<input class="file-upload-input" type='file'  name="filefoto" onchange="readURL(this);" accept="image/*" />
-								<div class="drag-text">
-									<h3>Drag and drop a file or select add Image</h3>
+			<div class="col-md-12" style="margin-top:50px">
+				<form action="<?php echo base_url()?>padmin/save_lampiran_foto" method="POST" enctype="multipart/form-data" >
+					<div class="col-md-4 col-md-offset-1">
+						<div class="col-md-12">
+							<div class="box">
+								<div class="boxhead">
+									<h3 class="boxttle">Foto Proyek</h3>
 								</div>
-							</div>
+								<div class="file-upload">
 
-							<div class="file-upload-content">
-								<img class="file-upload-image" src="#" alt="your image" />
-								<div class="image-title-wrap">
-									<button type="button" onclick="removeUpload()" class="remove-image">Remove <span class="image-title">Uploaded Image</span></button>
-								</div>
+									<div class="image-upload-wrap">
+										<input type="hidden" name="proyek_id" value="<?php echo $b['proyek_id']; ?>">
+										<input type="hidden"  name="jenis"  value="foto" class="form-control">	
+										<input class="file-upload-input" type='file' id="foto"  name="filefoto" onchange="readURL(this);" accept="image/*" />
+										<div class="drag-text">
 
-							</div>
-
-							<input type="submit" class="file-upload-btn" value="Simpan">
-
-						</form>
-
-					</div>
-				</div>
-			</div>
-
-
-			<div class="col-md-12">
-				<div class="box box-warning">
-					<div class="box-header with-border">
-						<h3 class="box-title">Dokumen Proyek</h3>
-					</div>
-					<div class="box-body">
-						<div class="row">
-							<form action="<?php echo base_url()?>padmin/save_lampiran_file" method="POST" enctype="multipart/form-data" >
-								<div class="form-group col-md-12">
-									<label>LAMPIRAN</label>
-									<input type="hidden" name="proyek_id" value="<?php echo $b['proyek_id']; ?>">
-									<input type="hidden"  name="jenis" value="file" class="form-control">
-									<input type="file"  name="fileat" class="form-control btn-success">
-								</div>
-								<div class="form-group col-md-12">
-									<div class="form-group pull-right">
-										<button type="submit" class="btn btn-primary btn-flat pull-right"><span class="fa fa-pencil"></span> Publish</button>
+											<h3><i class="fa fa-cloud-upload"></i><br>DROP FILES HERE <p>OR CLICK TO <span class="text-primary">BROWSE</span></p></h3>
+										</div>
 									</div>
+
+									<div class="file-upload-content">
+										<img class="file-upload-image" src="#" alt="your image" />
+										<div class="image-title-wrap">
+											<button type="button" onclick="removeUpload()" class="remove-image">Remove <span class="image-title">Uploaded Image</span></button>
+										</div>
+
+									</div>
+
 								</div>
-							</form>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col-md-6 col-md-offset-3">
+								<input type="submit" class="form-control btn btn-round btn-success padbtn" value="Simpan">
+							</div>
 						</div>
 					</div>
-				</div>
+				</form>
 
+				<form action="<?php echo base_url()?>padmin/save_lampiran_file" method="POST" enctype="multipart/form-data" >
+					<div class="col-md-4 col-md-offset-1">
+						<div class="col-md-12">
+							<div class="box">
+								<div class="boxhead2">
+									<h3 class="boxttle">Dokumen Proyek</h3>
+								</div>
+								<div class="file-upload">
+									<div class="image-upload-wrapss">
+										<input type="hidden" name="proyek_id" value="<?php echo $b['proyek_id']; ?>">
+										<input type="hidden"  name="jenis" value="file" class="form-control">
+										<input class="file-upload-inputss" type='file'  name="fileat" onchange="readFile(this);" accept="application/pdf,application/x-rar-compressed, application/octet-stream,application/zip, application/octet-stream, application/x-zip-compressed, multipart/x-zip" />
+										<div class="drag-text">
+
+											<h3><i class="fa fa-cloud-upload"></i><br>DROP FILES HERE <p>OR CLICK TO <span class="text-primary">BROWSE</span></p></h3>
+										</div>
+									</div>
+									<div class="file-upload-contentss">
+										<div class="image-titless-wrap">
+											<button type="button" onclick="removeUploadss()" class="remove-imagess">Remove <span class="image-titless">Uploaded Image</span></button>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6 col-md-offset-3">
+								<input type="submit" class="form-control btn btn-round btn-success padbtn" value="Simpan">
+							</div>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>
 	</section>
@@ -214,7 +327,7 @@ body {
 
 </body>
 </html>
-
+<script class="jsbin" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script type="text/javascript">
 	function readURL(input) {
 		if (input.files && input.files[0]) {
@@ -247,6 +360,41 @@ body {
 	});
 	$('.image-upload-wrap').bind('dragleave', function () {
 		$('.image-upload-wrap').removeClass('image-dropping');
+	});
+
+</script>
+<script type="text/javascript">
+	function readFile(input) {
+		if (input.files && input.files[0]) {
+
+			var reader = new FileReader();
+
+			reader.onload = function(e) {
+				$('.image-upload-wrapss').hide();
+
+				$('.file-upload-imagess').attr('src', e.target.result);
+				$('.file-upload-contentss').show();
+
+				$('.image-titless').html(input.files[0].name);
+			};
+
+			reader.readAsDataURL(input.files[0]);
+
+		} else {
+			removeUploadss();
+		}
+	}
+
+	function removeUploadss() {
+		$('.file-upload-inputss').replaceWith($('.file-upload-inputss').clone());
+		$('.file-upload-contentss').hide();
+		$('.image-upload-wrapss').show();
+	}
+	$('.image-upload-wrapss').bind('dragover', function () {
+		$('.image-upload-wrapss').addClass('image-droppingss');
+	});
+	$('.image-upload-wrapss').bind('dragleave', function () {
+		$('.image-upload-wrapss').removeClass('image-droppingss');
 	});
 
 </script>
